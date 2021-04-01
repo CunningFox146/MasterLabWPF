e1cc0a2c7b9e60768799879e812accf3f9a3c3ed
Артём <arti_2002_2011@mail.ru>
2021-04-01 23:05:45 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterLabWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Product> productList;
+        public MainWindow()
+        {
+            productList = new List<Product>();
+
+            
+            
+            InitializeComponent();
+
+            ProductGrid.ItemsSource = productList;
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var product = new Product(
+                ShortNameTB.Text,
+                FullNameTB.Text,
+                DescriptionTB.Name, //fix
+                CategoryLB.Text,
+                (int)RatingUDC.Value,
+                (int)PriceUDC.Value, 
+                null
+                );
+            productList.Add(product);
+
+            ProductGrid.Items.Refresh();
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MasterLabWPF
+{
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(string shortName, string fullName, string description, string category, int rating, int price, string imagesrc)
+        {
+            this.shortName = shortName;
+            this.fullName = fullName;
+            this.description = description;
+            this.category = category;
+            this.rating = rating;
+            this.price = price;
+            this.imagesrc = imagesrc;
+        }
+        
+        public string shortName { get; set; }
+        public string fullName { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public int rating { get; set; }
+        public int price { get; set; }
+        public string imagesrc { get; set; }
+        //public Image image { get; set; }
+
+
+    }
+}
